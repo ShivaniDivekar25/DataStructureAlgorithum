f741600e8867d46450f43db3f75333b6f319e05f
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-28 15:46:06 +0530

    Added Welcome To Data Structure Algorithum program

---
+++ DataStructureAlgorithum/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructureAlgorithum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure algorithum");
+        } 
+    }
+}
